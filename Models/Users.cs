14e44ec169e2e121ed5f57e8e490f251215d2568
andre <ikoto@et.esiea.fr>
2024-10-04 10:02:53 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace EXO2.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Usernname { get; set; }
+        public string Password { get; set; }
+    }
+}
